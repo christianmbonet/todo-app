2fae6676b9bd8141666a504ecda1986e4334ff89
Christian <christianm.bonet@gmail.com>
2022-02-28 20:00:14 -0500

    adding routes to server.js

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ require("dotenv").config();
 
 const app = express();
 const connectDB = require("./config/db");
+
+const todo = require("./routes/todo");
+
 connectDB();
 
+app.use("/api/todo", todo);
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("Server up and running"));
 
