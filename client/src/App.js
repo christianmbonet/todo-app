ea57b23a7bb3e8e58f33fb0f30457c51a0730465
Christian <christianm.bonet@gmail.com>
2022-03-01 22:28:30 -0500

    adding createTodo and showTodo, having errors with rendering blank page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import './App.scss';
+import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import { ShowTodoList } from "./components/showTodoList";
+import { CreateTodo } from "./components/createTodo";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-        <header>Hello, World!</header>
-    </div>
-  );
+export function App() {
+    return (
+        <div className="app-contents">
+            <Router>
+                <Route exact path="/" component={ShowTodoList} />
+                <Route path="/create-todo" component={CreateTodo} />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
