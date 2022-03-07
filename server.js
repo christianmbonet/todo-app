a7f7c8177d9f2876e8bfff8af156d0be5471b9e6
christian bonet <christianm.bonet@gmail.com>
2022-03-07 18:09:25 -0500

    everything working locally

--- server.js
+++ server.js
@@ -32,12 +32,3 @@ app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
 });
 
-
-if (process.env.NODE_ENV === 'production') {
-    // Handle React routing, return all requests to React app
-    app.get('*', (request, response) => {
-      response.sendFile(
-        path.resolve(__dirname, '..', 'client', 'build', 'index.html')
-      );
-    });
-  }
