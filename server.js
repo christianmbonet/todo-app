12a4a49e730389e1ae693f72a0ffd51c1070a1da
christian bonet <christianm.bonet@gmail.com>
2022-03-07 15:15:30 -0500

    updating port function

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get("/", (req, res) => res.send("Server up and running"));
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`);
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
