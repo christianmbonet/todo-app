5b6bbe64c41e34bfa1ab8d1a325b349608c0c055
christian bonet <christianm.bonet@gmail.com>
2022-03-07 17:59:24 -0500

    edits to server.js and to package.json

--- server.js
+++ server.js
@@ -21,16 +21,23 @@ app.use("/api/todo", todo);
 
 app.get("/", (req, res) => res.send("Server up and running"));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-}
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+  }
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
 });
+
+
+if (process.env.NODE_ENV === 'production') {
+    // Handle React routing, return all requests to React app
+    app.get('*', (request, response) => {
+      response.sendFile(
+        path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+      );
+    });
+  }
